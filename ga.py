d65fa8866f3f9b729fa16582b59df03bfe8e1fad
Zidz <->
2013-10-19 03:25:35 +0200

    Ah, the first real commit! Capable of evolving into a color (e.g ff ff ff). Next up: dumping the evolving colors to file for visual representation of the genetics.

--- ga.py
+++ ga.py
@@ -1,3 +1,6 @@
+import random
+
+
 class GAException(Exception):
 
     def __init__(self, value):
@@ -10,23 +13,149 @@ class GAException(Exception):
 class Gene():
 
     def __init__(self, *phenotype):
-        # phenotype = (trait, value)
-        if len(phenotype[0]) == 2:
-            self.phenotype = phenotype[0]
-        else:
-            raise GAException("".join(["Phenotype has to be of length 2,", 
-                str(len(phenotype)), " given"]))
+        self.phenotype = phenotype
+
+    def encode(self):
+        """Encode the genes to a bitstring"""
+        enc = ""
+        for genome in self.phenotype:
+            if isinstance(genome, str):
+                for c in genome:
+                    enc = "".join([enc, bin(ord(c))])
+            elif isinstance(genome, (int, hex)):
+                enc = "".join([enc, bin(genome)])
+        return enc[2:]
 
 
 class Chromosome():
 
-    def __init__(self, genes):
-        self.genotype = genes
+    def __init__(self, *genotype):
+        self.genotype = genotype
+        self.fitness = 0
 
-#phenotype
+    def encode(self):
+        """Encode the set of genes to a bitstring"""
+        enc = ""
+        for genome in self.genotype:
+            enc = "".join([enc, genome.encode()])
+        return enc
 
-def recombine(a, b):
-    pass
+    def __str__(self):
+        return repr(self.encode())
 
-def mutate(gene):
-    pass
+
+class GeneticAlgorithm():
+
+    def __init__(self, goal, popSize, mutrate = 10, crossrate = 70):
+        self.goal = goal
+        self.popSize = popSize
+        self.mutrate = mutrate
+        self.crossrate = crossrate
+
+        self._initPopulation()
+
+    def _initPopulation(self):
+        """creates the initial random population"""
+        population = []
+
+        for i in range(self.popSize):
+            bitstring = random.getrandbits(len(self.goal.encode()))
+            tmpChr = Chromosome(Gene(bitstring))
+            population.append(tmpChr)
+    
+        self.population = population
+
+    def decode(self, bitstring):
+        pass
+
+    def encode(self, chromosome):
+        pass
+
+    def evolve(self, procreations):
+        for i in range(procreations):
+            # Change to make better choices (higher fitness = higher chance to be picked)
+            roulette = []
+            fitSum = sum([x.fitness for x in self.population])
+            for i, ch in enumerate(self.population):
+                roulette += [i]*round(100*ch.fitness/fitSum)
+
+            index = roulette[random.randint(0, len(roulette)-1)]
+            a = self.population[index]
+            while index in roulette:
+                roulette.remove(index)
+
+            index = roulette[random.randint(0, len(roulette)-1)]
+            b = self.population[index]
+            #a, b = random.sample(self.population, 2)
+            ai = self.population.index(a)
+            bi = self.population.index(b)
+
+            if random.randint(0,100) < self.crossrate: 
+                a, b = self.crossover(a, b)
+
+            a = self.mutate(a)
+            b = self.mutate(b)
+
+            self.population[ai] = a
+            self.population[bi] = b
+
+    def crossover(self, a, b):
+        """mate the two chromosomes and return two children"""
+        rand = random.randint(1, 99)
+        crosspoint = round(len(self.goal.encode())*(rand/100))
+
+        a = a.encode()
+        b = b.encode()
+
+        # Prevent the chromosomes from shrinking
+        goal = self.goal.encode()
+        a = list(a)
+        b = list(b)
+        while len(a) < len(goal):
+            a = ['0'] + a
+        while len(b) < len(goal):
+            b = ['0'] + b
+
+        a = "".join(a)
+        b = "".join(b)
+
+        achild = Chromosome(Gene(int(a[:crosspoint]+b[crosspoint:], 2)))
+        bchild = Chromosome(Gene(int(b[:crosspoint]+a[crosspoint:], 2)))
+
+        return (achild, bchild)
+
+    def mutate(self, chromosome):
+        """flip a random bit in the chromosome"""
+        mut = list(chromosome.encode())
+        while len(mut) < len(self.goal.encode()):
+            mut = ['0'] + mut
+
+        for i in range(len(mut)):
+            if random.randint(0,100) < self.mutrate:
+                mut[i] = '0' if i == '1' else '1'
+
+        mut = "".join(mut)
+        return Chromosome(Gene(int(mut, 2)))
+
+    def fit(self):
+        """adjust the fitness of each chromosome"""
+        for ch in self.population:
+            goal = int(self.goal.encode(), 2)
+            chSc = int(ch.encode(), 2)
+            ch.fitness = goal/(chSc if chSc != 0 else 1)
+
+    def run(self):
+        """let mother nature have its way"""
+        for r in range(1000):
+            self.fit()
+            purities = [x.fitness for x in self.population]            
+            if 1.0 in purities:
+                index = purities.index(1.0)
+                print("finished on generation:", r)
+                print("current generation: ", [x.encode() for x in self.population])
+                return self.population[index]
+
+            self.evolve(1)
+
+        print([x.encode() for x in self.population])
+        return "no result..."
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from ga import *
 
 
 def main():
-    a = Gene(("color", 0xffffff))
-    b = Gene(("color", 0x000000))
-    ch = Chromosome([a, b])
-    print(ch.genotype)
-
+    ga = GeneticAlgorithm(Chromosome(Gene(0xffffff)), 20, 10, 85)
+    
+    a = ga.run()
+    print(a)
+    #print([x.encode() for x in ga.population])
 
 if __name__ == '__main__':
     main()
